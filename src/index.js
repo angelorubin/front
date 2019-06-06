5e9ef0c3be98b9cc6e52ec363717e5b76fac50b0
angelorubin <angelorubin@gmail.com>
2019-06-06 13:57:57 -0300

    Transferindo BrowserRouter e CssBaseline para o componente App.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import React from 'react';
-import { render } from 'react-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
+import React from "react";
+import { render } from "react-dom";
+import { App } from "./components/App";
 
-render(
-  <>
-    <BrowserRouter>
-      <Home />
-      <CssBaseline />
-    </BrowserRouter>
-  </>,
-  document.querySelector('#root'),
-);
+render(<App />, document.querySelector("#root"));
