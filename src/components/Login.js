6bccf50108cb90462e526225b4cb9c3e37afad38
angelorubin <angelorubin@gmail.com>
2019-06-07 15:34:52 -0300

    Organizando ciclo de vida, funções e css do componente

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,60 @@
 import React from "react";
 import styled from "styled-components";
 import * as Yup from "yup";
-import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import Snackbar from "@material-ui/core/Snackbar";
 import SnackbarContent from "@material-ui/core/SnackbarContent";
 import { Form, Input } from "@rocketseat/unform";
 import { fakeAuth } from "../utils";
+
+const Login = props => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const { history } = props;
+
+  const handleSubmit = (data, { resetForm }) => {
+    setOpen(true);
+
+    // fake auth
+    fakeAuth.authenticate(true);
+
+    fakeAuth.isAuthenticated
+      ? setInterval(() => {
+          history.push("/dashboard");
+        }, 2000)
+      : history.push("/");
+  };
+
+  return (
+    <Wrapper>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center"
+        }}
+        open={open}
+        autoHideDuration={2000}
+      >
+        <SnackbarContent
+          message={"logado com sucesso !"}
+          className={classes.root}
+        />
+      </Snackbar>
+      <StyledForm schema={schema} onSubmit={handleSubmit}>
+        <StyledTitle>Octo EAD</StyledTitle>
+        <StyledInputEmail name="email" placeholder="nome de usuário ou email" />
+        <StyledInputPassword
+          name="password"
+          placeholder="senha"
+          type="password"
+        />
+        <StyledEnterButton type="submit">entrar</StyledEnterButton>
+        <StyledLostPassword href="/">esqueci minha senha</StyledLostPassword>
+        <StyledNewUser href="/">cadastrar novo usuário</StyledNewUser>
+      </StyledForm>
+    </Wrapper>
+  );
+};
 
 const schema = Yup.object().shape({
   email: Yup.string()
@@ -74,164 +122,4 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const MySnackbarContentWrapper = props => {
-  const classes = useStyles();
-  const { className, message, onClose, variant, ...other } = props;
-
-  return (
-    <SnackbarContent
-      className={clsx(classes[variant], className)}
-      message={
-        <span id="client-snackbar" className={classes.message}>
-          {message}
-        </span>
-      }
-    />
-  );
-};
-
-const Login = props => {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const { history } = props;
-
-  const handleSubmit = (data, { resetForm }) => {
-    setOpen(true);
-
-    // fake auth
-    fakeAuth.authenticate(true);
-
-    fakeAuth.isAuthenticated
-      ? setInterval(() => {
-          history.push("/dashboard");
-        }, 2000)
-      : history.push("/");
-  };
-
-  return (
-    <Wrapper>
-      <Snackbar
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "center"
-        }}
-        open={open}
-        autoHideDuration={2000}
-      >
-        <SnackbarContent message={"logado com sucesso !"} className={classes.root} />
-      </Snackbar>
-      <StyledForm schema={schema} onSubmit={handleSubmit}>
-        <StyledTitle>Octo EAD</StyledTitle>
-        <StyledInputEmail name="email" placeholder="nome de usuário ou email" />
-        <StyledInputPassword
-          name="password"
-          placeholder="senha"
-          type="password"
-        />
-        <StyledEnterButton type="submit">entrar</StyledEnterButton>
-        <StyledLostPassword href="/">esqueci minha senha</StyledLostPassword>
-        <StyledNewUser href="/">cadastrar novo usuário</StyledNewUser>
-      </StyledForm>
-    </Wrapper>
-  );
-};
-
 export default Login;
-
-/*
-const styles = {
-  root: {
-    backgroundColor: "#ff8b61",
-    "grid-gap": "1rem",
-    display: "grid",
-    "grid-template-columns": "1fr",
-    "justify-items": "center",
-    "align-items": "center",
-    height: "100vh"
-  },
-  form: {
-    display: "grid",
-    "justify-items": "center",
-    backgroundColor: "white",
-    padding: "1rem",
-    "border-radius": "5px",
-    width: "300px"
-  },
-  btnEnter: {
-    margin: "2rem",
-    backgroundColor: "#ff8b61",
-    color: "white",
-    width: "100%",
-    "&:hover": {
-      "background-color": "black",
-      color: "white"
-    }
-  },
-  btnLostPassword: {
-    backgroundColor: "transparent",
-    width: "100%",
-    "&:hover": {
-      "background-color": "black",
-      color: "white"
-    }
-  },
-  title: {
-    "font-weight": "bold",
-    color: "#ff8b61"
-  },
-  email: {
-    width: "100%"
-  }
-};
-
-class Login extends Component {
-  state = {
-    email: "",
-    password: ""
-  };
-  handleInput = e => {
-    this.setState({ [e.target.id]: e.target.value });
-  };
-  render() {
-    const { classes } = this.props;
-    const { email, password } = this.state;
-    return (
-      <>
-        <div className={classes.root}>
-          <form className={classes.form}>
-            <img src={logo} width="80" />
-            <Typography className={classes.title} variant="h4">
-              Octo
-            </Typography>
-            <TextField
-              id="email"
-              label="email"
-              className={classes.email}
-              value={email}
-              onChange={this.handleInput}
-              margin="normal"
-            />
-            <TextField
-              id="password"
-              label="password"
-              type="password"
-              className={classes.email}
-              value={password}
-              onChange={this.handleInput}
-              margin="normal"
-            />
-            <Button size="small" className={classes.btnEnter}>
-              Entrar
-            </Button>
-            <Button size="small" className={classes.btnLostPassword}>
-              Esqueci a senha
-            </Button>
-          </form>
-        </div>
-      </>
-    );
-  }
-}
-
-export default withStyles(styles)(Login);
-*/
