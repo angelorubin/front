5d412d31eefa4a8b853c64969590f45bc672921b
angelorubin <angelorubin@gmail.com>
2019-06-08 15:00:01 -0300

    Criando estrutura básica do componente Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { height } from "@material-ui/system";
+import styled from "styled-components";
+import { makeStyles } from "@material-ui/styles";
 
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <Wrapper>
+      <WrapperLogoMenu>
+        <StyledLogo>Logo</StyledLogo>
+        <StyledMenu>Menu</StyledMenu>
+      </WrapperLogoMenu>
+      <WrapperSidebarContent>
+        <div>Sidebar</div>
+        <div>Content</div>
+      </WrapperSidebarContent>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+`;
+
+const WrapperLogoMenu = styled.div`
+  display: grid;
+  grid-template-columns: 30% 1fr;
+`;
+
+const WrapperSidebarContent = styled.div`
+  display: grid;
+  grid-template-columns: 30% 1fr;
+`;
+
+const StyledLogo = styled.div`
+  background-color: pink;
+`;
+
+const StyledMenu = styled.div`
+  background-color: gray;
+`;
 
 export default Dashboard;
