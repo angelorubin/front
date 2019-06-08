bfb16d28ed68c960ba4f5f433ce64bbab7bcae5d
angelorubin <angelorubin@gmail.com>
2019-06-08 14:58:10 -0300

    Exportando componente com export default

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
-
 import Login from "./login";
 import Dashboard from "./dashboard";
 import PrivateRoute from "./privateRoute";
 
-export const App = () => {
+const App = () => {
   return (
     <div>
       <CssBaseline />
       <Router>
+        {/*
         <Route exact path="/" component={Login} />
         <PrivateRoute path="/dashboard" component={Dashboard} />
+        */}
+        <Dashboard />
       </Router>
     </div>
   );
 };
+
+export default App;
